faf27e383afc25d261560e6339ac7bdc73a3fac2
EnzyI <zyxrywsp@gmail.com>
2026-01-16 00:03:44 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,39 +4,29 @@
 use core::arch::global_asm;
 use core::panic::PanicInfo;
 
-// Đoạn mã Assembly khởi động quan trọng nhất
+// Dùng lệnh Thumb ngay từ đầu để khớp với target của bro
 global_asm!(
-    ".arm",                    // Bắt đầu ở chế độ ARM 32-bit (mặc định của QEMU)
     ".section .vector_table, \"ax\"",
     ".global _reset",
+    ".thumb",                  // Ép dùng Thumb mode
     "_reset:",
-    "add r0, pc, #1",          // Thủ thuật: lấy địa chỉ hiện tại + 1 để bật cờ Thumb
-    "bx r0",                   // Nhảy đến r0 và chuyển CPU sang chế độ Thumb
-    
-    ".thumb",                  // Từ đây code sẽ chạy ở chế độ Thumb (như Rust build)
-    ".global _start_thumb",
-    "_start_thumb:",
-    "ldr r0, =_start",         // Nạp địa chỉ hàm _start của Rust
-    "blx r0",                  // Nhảy vào hàm Rust
+    "ldr r0, =_start",         // Nạp địa chỉ hàm _start
+    "blx r0",                  // Nhảy vào Rust
     ".align 4"
 );
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // Địa chỉ cổng Serial (UART0) của máy VersatilePB
+    // UART0 của VersatilePB
     let uart = 0x101f_1000 as *mut u8;
-
-    // Chuỗi thông báo "sống sót"
     let msg = b"ALIVE AND RUNNING!\n";
 
     for &byte in msg {
         unsafe {
-            // Ghi từng byte vào UART để hiện lên Terminal
             core::ptr::write_volatile(uart, byte);
         }
     }
 
-    // Vòng lặp vô tận để CPU không bị hoảng loạn (panic)
     loop {}
 }
 
