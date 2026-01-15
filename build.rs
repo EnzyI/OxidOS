4e89614e155652ecb198d01f3b4a82909954a3ae
EnzyI <zyxrywsp@gmail.com>
2026-01-15 23:10:48 +0700

    Delete build.rs

--- build.rs
+++
@@ -1,9 +0,0 @@
-fn main() {
-    // Chỉ định file assembly cần biên dịch
-    cc::Build::new()
-        .file("src/boot.s")
-        .compile("boot");
-
-    // Báo cho Cargo biết nếu boot.s thay đổi thì cần build lại
-    println!("cargo:rerun-if-changed=src/boot.s");
-}
