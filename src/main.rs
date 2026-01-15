33048c479406adcc93df0d196ed4778bf206acf6
EnzyI <zyxrywsp@gmail.com>
2026-01-15 18:49:58 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,18 @@
 #![no_std]
 #![no_main]
+#![feature(compiler_builtins)] // Mở khóa tính năng đặc biệt của Nightly
 
 use core::panic::PanicInfo;
 
-// Khi OxidOS gặp lỗi không thể hồi phục
+// Hàm xử lý lỗi nghiêm trọng
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-// Điểm khởi đầu thực sự của OxidOS
+// Điểm khởi đầu của nhân hệ điều hành
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // Mã nguồn điều khiển CPU sẽ nằm ở đây
+    // OxidOS bắt đầu ở đây
     loop {}
 }
