d4416a0ba26b04a0ea2f39231550a43271648ef1
EnzyI <zyxrywsp@gmail.com>
2026-01-15 20:49:58 +0700

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,8 +1,9 @@
 fn main() {
-    // Bảo Rust build file assembly
-    println!("cargo:rerun-if-changed=src/boot.s");
+    // Chỉ định file assembly cần biên dịch
     cc::Build::new()
-        .asm_arch("armv7-a")
         .file("src/boot.s")
         .compile("boot");
+
+    // Báo cho Cargo biết nếu boot.s thay đổi thì cần build lại
+    println!("cargo:rerun-if-changed=src/boot.s");
 }
