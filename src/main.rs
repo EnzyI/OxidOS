64154bb4016692eb4e330aaf47c6022e2313b5fd
EnzyI <zyxrywsp@gmail.com>
2026-01-15 19:41:34 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,31 @@
 #![no_std]
 #![no_main]
-#![feature(compiler_builtins)] // Mở khóa tính năng đặc biệt của Nightly
+#![feature(compiler_builtins)]
 
 use core::panic::PanicInfo;
 
-// Hàm xử lý lỗi nghiêm trọng
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-// Điểm khởi đầu của nhân hệ điều hành
+// Hàm gửi một ký tự ra cổng Serial
+fn print_char(c: u8) {
+    // Địa chỉ thanh ghi dữ liệu của UART (thay đổi tùy chip, đây là ví dụ STM32)
+    let uart_dr = 0x4001_1004 as *mut u8;
+    unsafe {
+        // Ghi trực tiếp vào bộ nhớ phần cứng
+        *uart_dr = c;
+    }
+}
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // OxidOS bắt đầu ở đây
+    let message = b"OxidOS online!";
+    
+    for &byte in message {
+        print_char(byte);
+    }
+
     loop {}
 }
