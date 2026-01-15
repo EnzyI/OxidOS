9e996d55406f031bbb76be74f8a11e60d9747cab
EnzyI <zyxrywsp@gmail.com>
2026-01-15 23:28:23 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,19 @@
 use core::arch::global_asm;
 use core::panic::PanicInfo;
 
-// Đưa assembly "mồi" trực tiếp vào đây
 global_asm!(
+    ".arm",
     ".section .vector_table, \"ax\"",
     ".global _reset",
     "_reset:",
-    "b _start"
+    "ldr pc, =_start", // Lệnh nhảy này sẽ không bắt đầu bằng byte 00
+    ".align 4"
 );
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // Ghi trực tiếp vào UART của máy VersatilePB
     let uart = 0x101f_1000 as *mut u8;
-    for &byte in b"ALIVE" {
+    for &byte in b"ALIVE\n" {
         unsafe { core::ptr::write_volatile(uart, byte); }
     }
     loop {}
