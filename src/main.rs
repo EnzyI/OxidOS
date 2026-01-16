a6fad33dd0bf477cf76038b6ad8a6052879391ec
EnzyI <zyxrywsp@gmail.com>
2026-01-16 12:18:36 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,24 @@
 
 use core::panic::PanicInfo;
 
+// Định nghĩa bảng Vector Table ở đầu file
+#[link_section = ".vector_table"]
+#[no_mangle]
+pub static MS_VECTOR_TABLE: [u32; 2] = [
+    0x2000_5000,    // 1. Initial Stack Pointer (Địa chỉ cuối RAM)
+    _start as u32,  // 2. Reset Handler (Địa chỉ hàm bắt đầu)
+];
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    // Địa chỉ UART0 của máy Stellaris LM3S6965
-    let uart0 = 0x4000_c000 as *mut u8;
+    let uart0 = 0x4000_c000 as *mut u8; // UART cho máy lm3s
 
     loop {
-        for &byte in b"ALIVE ON LM3S!\n" {
+        for &byte in b"ALIVE AT LAST!\n" {
             unsafe {
                 core::ptr::write_volatile(uart0, byte);
             }
         }
-        // Vòng lặp chờ để không làm treo terminal điện thoại
         for _ in 0..1000000 { core::hint::spin_loop(); }
     }
 }
