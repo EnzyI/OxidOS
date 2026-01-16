f85e5ba0bcf0204411bedc1735ae7a9fde19f747
EnzyI <zyxrywsp@gmail.com>
2026-01-16 12:27:43 +0700

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,23 +3,16 @@
 
 use core::panic::PanicInfo;
 
-// Bảng Vector Table dùng u32 để trình biên dịch không báo lỗi pointer
-#[link_section = ".vector_table"]
-#[no_mangle]
-pub static MS_VECTOR_TABLE: [u32; 2] = [
-    0x2000_5000,               // 1. Initial Stack Pointer
-    _reset_handler as u32,     // 2. Reset Handler (Địa chỉ hàm khởi động)
-];
-
 #[no_mangle]
 pub extern "C" fn _reset_handler() -> ! {
-    let uart0 = 0x4000_c000 as *mut u8; // UART cho máy LM3S
+    // UART0 cho máy Stellaris LM3S
+    let uart0 = 0x4000_c000 as *mut u8;
 
     loop {
         for &byte in b"ALIVE AT LAST!\n" {
             unsafe { core::ptr::write_volatile(uart0, byte); }
         }
-        // Delay để terminal điện thoại kịp hiển thị
+        // Vòng lặp chờ để Terminal không bị lag
         for _ in 0..1000000 { core::hint::spin_loop(); }
     }
 }
